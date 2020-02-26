3d00bbc94a32ddc068828db53781104533e48268
Ty Hobbs <43939086+tjhobbs1@users.noreply.github.com>
2020-02-26 13:22:04 -0600

    Update helloGitHub.py
    New Method Added

--- helloGitHub.py
+++ helloGitHub.py
@@ -1,6 +1,10 @@
 def printHello():
     return f'Hello Git Hub'
 
+def printName(name):
+    return f'Hello {name}'
+
 
 if __name__ == '__main__':
     print(printHello())
+    print(printName('Ty'))
